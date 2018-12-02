b7de87a1f39b94bc001e46651da592d9879f3735
Toshiyuki Saito <toshiyuki.saito.16@gmail.com>
2018-12-02 10:26:14 +0900

    ゲームディレクトリを決めうちに変更。

--- forge.sh
+++ forge.sh
@@ -1,4 +1,5 @@
 FORGE_VERSION='1.8.9-forge1.8.9-11.15.1.1722'
+GAME_DIRECTORY="${HOME}/computercraftedu"
 
 curl -o forge.jar "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8.9-11.15.1.1722/forge-1.8.9-11.15.1.1722-installer.jar"
 curl -o ComputerCraftPlusComputerCraftEdu1.79.jar "http://computercraftedu.com/downloads/ComputerCraftPlusComputerCraftEdu1.79.jar"
@@ -8,12 +9,8 @@ echo "clientをインストールしてください。"
 
 java -jar forge.jar
 
-echo "インストール先をここにドラッグ＆ドロップしてください。"
-echo -n ":"
-read gamedirectory
-
-mkdir -p "$gamedirectory/mods/"
-cp "ComputerCraftPlusComputerCraftEdu1.79.jar" "$gamedirectory/mods/"
-ruby make_gamedir.rb 'cumputerfraftedu' "$FORGE_VERSION" "$gamedirectory"
+mkdir -p "$GAME_DIRECTORY/mods/"
+cp "ComputerCraftPlusComputerCraftEdu1.79.jar" "$GAME_DIRECTORY/mods/"
+ruby make_gamedir.rb 'cumputerfraftedu' "$FORGE_VERSION" "$GAME_DIRECTORY"
 
 echo "完了."
